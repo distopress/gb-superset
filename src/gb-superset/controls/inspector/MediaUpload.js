0bd8dfc585502d28d661d862e72aaca02ccff4d4
Sagor Rana <60606778+sagorranait@users.noreply.github.com>
2024-03-02 21:48:24 +0600

    Resolve Error

--- src/gb-superset/controls/inspector/MediaUpload.js
+++ src/gb-superset/controls/inspector/MediaUpload.js
@@ -1,19 +1,17 @@
 import { Button } from '@wordpress/components';
-import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
+import { MediaUpload } from '@wordpress/block-editor';
 
 import { ControlContainer } from '@gb-superset/supports/control';
 
 export default (props) => {
   return (
     <ControlContainer {...props} valueProp={'value'} changeProp={'onSelect'} className="gb-superset-media-upload">
-      <MediaUploadCheck>
         <MediaUpload
           allowedTypes={[ 'image', 'video', 'audio' ]}
           render={ ( { open } ) => (
             <Button variant="primary" onClick={ open }>Open Media Library</Button>
           ) }
         />
-      </MediaUploadCheck>
     </ControlContainer>
   );
 }
