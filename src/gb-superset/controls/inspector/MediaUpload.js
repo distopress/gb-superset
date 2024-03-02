420625f27289fcd5ae6821e396e63aee61a6edc3
Sagor Rana <60606778+sagorranait@users.noreply.github.com>
2024-03-02 15:49:54 +0600

    MediaUpload Error is solved

--- src/gb-superset/controls/inspector/MediaUpload.js
+++ src/gb-superset/controls/inspector/MediaUpload.js
@@ -1,19 +1,17 @@
 import { Button } from '@wordpress/components';
-import { MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
+import { MediaUpload } from '@wordpress/block-editor';
 
 import { ControlContainer } from '@gb-superset/supports/control';
 
 export default (props) => {
   return (
-    <ControlContainer {...props} valueProp={'value'} changeProp={'onSelect'} className="gb-superset-number-control">
-      <MediaUploadCheck>
+    <ControlContainer {...props} valueProp={'value'} changeProp={'onSelect'} className="gb-superset-media-upload">
         <MediaUpload
           allowedTypes={[ 'image', 'video', 'audio' ]}
           render={ ( { open } ) => (
             <Button variant="primary" onClick={ open }>Open Media Library</Button>
           ) }
         />
-      </MediaUploadCheck>
     </ControlContainer>
   );
 }
